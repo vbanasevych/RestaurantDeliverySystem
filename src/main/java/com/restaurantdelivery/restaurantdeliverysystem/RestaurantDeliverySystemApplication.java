ec93dd6e217074c62f44db0248ac2cedb1f660c7
vbanasevych <viktoriabanasevic@gmail.com>
2025-05-09 21:56:37 +0300

    merge conflicts solving

--- src/main/java/com/restaurantdelivery/restaurantdeliverysystem/RestaurantDeliverySystemApplication.java
+++ src/main/java/com/restaurantdelivery/restaurantdeliverysystem/RestaurantDeliverySystemApplication.java
@@ -1,11 +1,21 @@
 package com.restaurantdelivery.restaurantdeliverysystem;
 
+import com.restaurantdelivery.restaurantdeliverysystem.h2.Dish;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class RestaurantDeliverySystemApplication {
+public class RestaurantDeliverySystemApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        Dish dish = new Dish();
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RestaurantDeliverySystemApplication.class, args);
     }
+
 }
