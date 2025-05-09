14e9e1921509dfc2f69fa59f29188fc1cf193670
OlhenShu <olya.shutileva@gmail.com>
2025-05-09 17:56:28 +0300

    fix config

--- src/main/java/com/restaurantdelivery/restaurantdeliverysystem/RestaurantDeliverySystemApplication.java
+++ src/main/java/com/restaurantdelivery/restaurantdeliverysystem/RestaurantDeliverySystemApplication.java
@@ -4,6 +4,7 @@ import com.restaurantdelivery.restaurantdeliverysystem.h2.Dish;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class RestaurantDeliverySystemApplication implements CommandLineRunner {
